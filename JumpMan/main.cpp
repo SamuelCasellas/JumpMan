5eaacf7dc9a722a16fc4b1ee193e623c48518f04
Samuel Casellas <sam.casellas@gmail.com>
2023-03-14 17:53:01 -0600

    Initial Commit

---
+++ JumpMan/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  JumpMan
+//
+//  Created by Samuel on 3/14/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
